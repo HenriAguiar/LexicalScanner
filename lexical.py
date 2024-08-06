712f6c753054947f2fc77ae2cffacb939bcc6d36
HenriAguiar <aguiar.henri1@gmail.com>
2024-08-06 15:29:44 -0300

    Feat: Adiciona verificação de tokens válidos

--- lexical.py
+++ lexical.py
@@ -1,31 +1,50 @@
-texto = "while     i < 100 do i =i + j;"
+import csv
+
+texto = "while   g for  i < 100 do i =i + j;"
 
 def tokenizador(entrada):
     tokens = {}
     tokenAtual = ""
     i = 0
-    
     while i < len(entrada):
         char = entrada[i]
-        
         if char in (" ", ";", "<", ">", "=", "-", "+"):
+            tipo=verificaToken(tokenAtual)
             if tokenAtual:
-                tokens[i-1]=tokenAtual
-                tokenAtual = ""
-                
-            if char != " ":
+                if tipo:
+                    tokens[i-1]=tokenAtual
+                    tokenAtual = ""
+                else : 
+                    print(f"Token '{tokenAtual}' não identificado na posição {i} da linha 0")
+                    tokenAtual = ""
+            elif char != " ":
                 tokens[i]=char
         else:
             tokenAtual += char
-        
         i += 1
-    
     if tokenAtual:
-        tokens[i]=tokenAtual
-    
+        if verificaToken(tokenAtual):
+            tokens[i]=tokenAtual
+        else:
+            print(f"Token '{tokenAtual}' não identificado na posição {i} da linha 0")
     return tokens
 
+def is_number(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+    
 def verificaToken(token):
+    tokens={"reservadas":("while","do"),
+            "operadores":("<","=","+"),
+            "terminador":(";"),
+            "identificador":("i","j")}
+    for tipo,valores in tokens.items():
+        if token in valores or is_number(token):
+            return tipo
+    return False
 
 
 print(tokenizador(texto))
